40125e653ebc18753f71654d7acf4f98e1d6221a
Dwayne Kurfirst <urdragon2@Dwaynes-Mac-mini.local>
2017-07-17 08:00:06 -0500

    Initial Commit

---
+++ DexterApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DexterApp
+//
+//  Created by Dwayne Kurfirst on 7/17/17.
+//  Copyright © 2017 kurfirstcorp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
